dd51e920ef53443388a07ff6c330258add47e0c1
Kevin Dean <audioman95@gmail.com>
2011-09-26 14:17:38 -0500

    added fullscreen mode option to settings.json

--- main.cpp
+++ main.cpp
@@ -11,9 +11,14 @@
 int main(){
 	json_spirit::mObject settings;
 	ReadSettings(&settings);
+	bool fullscreenMode = true;
+	json_spirit::mValue fullscreenCheck = settings["fullscreen"];
+	if(fullscreenCheck.type() == json_spirit::bool_type)
+		fullscreenMode = fullscreenCheck.get_bool();
 	
-	sf::RenderWindow App(sf::VideoMode(900, 900*sf::VideoMode::GetDesktopMode().Height/sf::VideoMode::GetDesktopMode().Width), "Magnatar", sf::Style::Close);
-	//sf::Style::Fullscreen
+	sf::RenderWindow App(sf::VideoMode(900, 900*sf::VideoMode::GetDesktopMode().Height/sf::VideoMode::GetDesktopMode().Width), "Magnatar", 
+						 (fullscreenMode ? sf::Style::Close|sf::Style::Fullscreen : sf::Style::Close));
+	
 	App.SetFramerateLimit(120);
 	App.PreserveOpenGLStates(true);
 	
